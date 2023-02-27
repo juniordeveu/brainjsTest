a70aca231b34b426f5f7a6a730dc8d4bb7a2bf06
juniordeveu <tomrys@juniordev.eu>
2023-02-27 08:17:39 +0100

    testing net xor

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const DataTrening = require('./public/test')
 
 /* brain js */
 const brain = require( 'brain.js' )
+
 const getDataToTrening = new DataTrening
 
 /* end */
@@ -17,14 +18,28 @@ const train = () => {
         log:true
     } )
 }
+
 do {
     train()
 
 } while ( net.run( [1,0] ) < .997 );
 
-console.log("testuje " , net.run( [1,0] ) )
+console.log("testuje  " , net.run( [1,0] ) )
 
+/* testing  */
 
+getDataToTrening.testTrainingData.forEach( data => {
+    const output = net.run( data.input )
+   
+    if(data.output != Math.round( output )){
+        console.log(` ERROR ->  Input: ${ data.input } | Expected output: ${ data.output } | Actual output: ${ output } ${ Math.round( output ) } `);
+    } else {
+        console.log(`Input: ${ data.input } | Expected output: ${ data.output } | Actual output: ${ output } ${ Math.round( output ) } `); 
+    }
+    
+} )
+
+/* end */
 
 /* save and load */
 const result = net.toJSON()
@@ -33,11 +48,10 @@ fs.writeFile('result', JSON.stringify( result ), ( err ) => {
 })
 /* end */
 
-app.get( '/', ( req, res) => {
+app.get( '/', ( req, res ) => {
 
 
     res.json( result )
-    
     res.end()
 
 } ) 
--- public/test.js
+++ public/test.js
@@ -7,6 +7,15 @@ class DataTrening {
         { input:[1,0] , output:[1] },
         { input:[1,1] , output:[0] },
     ]
+    testTrainingData = [
+        { input:[0,0] , output:[0] },
+        { input:[0,1] , output:[1] },
+        { input:[1,0] , output:[1] },
+        { input:[1,1] , output:[1] },
+        { input:[0,1] , output:[1] },
+        { input:[1,1] , output:[0] },
+        { input:[1,1] , output:[1] },
+    ]
     configBrain = {
         hiddenLayers:[3 ],
         activation: 'sigmoid',
@@ -14,6 +23,7 @@ class DataTrening {
         logPeriod: 500 ,
         iterations:500
     }
+
     
 }
 
